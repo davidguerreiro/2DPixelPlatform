ca5d29c9327ec79356a91c51c667dc5ddd07a009
davidguerreiro <me@davidguerreiro.com>
2019-12-26 17:07:54 +0000

    add some extra basic code in the player controller class

--- Player/PlayerController.cs
+++ Player/PlayerController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float moveSpeed;                             // Player's movement speed.
+    private Rigidbody2D myRigibody;                     // Rigibody2D component reference.
+
+    // Start is called before the first frame update.
+    void Start() {
+        Init();
+    }
+
+    // Update is called once per frame.
+    void Update() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void MovePlayer() {
+
     }
 
-    
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    /// <returns>void</returns>
+    public void Init() {
+
+        // get rigibody 2D component reference.
+        myRigibody = GetComponent<Rigidbody2D>();
+    }
+
 }
