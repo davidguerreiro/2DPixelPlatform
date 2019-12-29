43bc4ca928fb5debce412e3f3c2df1972788a339
davidguerreiro <me@davidguerreiro.com>
2019-12-29 22:57:09 +0000

    add coin scritp

---
+++ Collectibles/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public int coinValue;                           // Coin value - different kinds of coins might gran higer points.
+    
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D( Collider2D other ) {
+        
+        // collect coin.
+        if ( other.tag == "Player" && other.GetComponent<PlayerController>().IsPlayerActive() ) {
+            
+            // add coin to the count.
+            LevelManager.instance.AddCoins( this.coinValue );
+            Destroy( this.gameObject );
+        }
+    }
+}
--- Managers/LevelManager.cs
+++ Managers/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour {
     public static LevelManager instance;                // Public static instance of this class.
     public float waitToRespawn;                         // Time to wait before the player respawn.
     public PlayerController thePlayer;                  // Player controller class component reference.
+    public int coinsCount;                              // Total number of coins collected.
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -44,10 +45,23 @@ public class LevelManager : MonoBehaviour {
     }
 
     /// <summary>
+    /// Add coins to our
+    /// internal coins count.
+    /// </summary>
+    /// <param name="coinsToAdd">int - coints to be added</param>
+    /// <returns>void</returns>
+    public void AddCoins( int coinsToAdd ) {
+        this.coinsCount += coinsToAdd;
+        Debug.Log( this.coinsCount );
+    }
+
+    /// <summary>
     /// Init class method.
     /// </summary>
     /// <returns>void</returns>
     public void Init() {
-        // thePlayer = FindObjectOfType<PlayerController>();
+        
+        // set default values for attributes.
+        this.coinsCount = 0;
     }
 }
