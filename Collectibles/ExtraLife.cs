ec3528784e77355c00eaa3b038af77cf81ff279b
davidguerreiro <me@davidguerreiro.com>
2020-01-12 15:44:48 +0000

    add life collectible

---
+++ Collectibles/ExtraLife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : MonoBehaviour {
+    public int lifesToGive;                                 // How many lives this item grants to the player.
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other) {
+
+        // add the life to the player.
+        if ( other.tag == "Player" ) {
+            PlayerController.instance.UpdateLifes( this.lifesToGive );
+            Destroy( this.gameObject );
+        }
+    }
+}
