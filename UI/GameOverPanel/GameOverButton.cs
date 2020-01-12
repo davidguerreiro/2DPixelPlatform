f5ba1b8ec33d804e5e7045caffd89d634c90d364
davidguerreiro <me@davidguerreiro.com>
2020-01-12 17:51:59 +0000

    complete game over panel

---
+++ UI/GameOverPanel/GameOverButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButton : MonoBehaviour {
+
+    /// <summary>
+    /// Reload current scene.
+    /// </summary>
+    /// <returns>void</returns>
+    public void RestartScene() {
+        SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+    }
+
+    /// <summary>
+    /// Close game.
+    /// </summary>
+    /// <returns>void</returns>
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
