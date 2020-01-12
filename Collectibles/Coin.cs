f7856abe4d8f8ac1af86efe1667eb4dfccdb0195
davidguerreiro <me@davidguerreiro.com>
2020-01-12 19:23:02 +0000

    add rotate coins feature

--- Collectibles/Coin.cs
+++ Collectibles/Coin.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Coin : MonoBehaviour {
 
     public int coinValue;                           // Coin value - different kinds of coins might gran higer points.
+    public float rotateSpeed;                       // Coin rotate speed.
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate() {
+        RotateCoin();
+    }
     
     /// <summary>
     /// Sent when another object enters a trigger collider attached to this
@@ -20,5 +28,15 @@ public class Coin : MonoBehaviour {
             LevelManager.instance.AddCoins( this.coinValue );
             Destroy( this.gameObject );
         }
+    }
+
+    /// <summary>
+    /// Rotate coin.
+    /// This method must be called
+    /// at FixedUpdate.
+    /// </summary>
+    /// <returns>void</returns>
+    private void RotateCoin() {
+        transform.Rotate( 0f, this.rotateSpeed * Time.deltaTime, 0f );
     }
 }
